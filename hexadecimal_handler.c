64527717bb4859271bbe51a27e6abb2e69dd69bd
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 01:57:32 +0300

    Update hexadecimal_handler.c

---
+++ hexadecimal_handler.c
@@ -0,0 +1,27 @@
+#include "main.h"
+int handle_hex(va_list args, int uppercase)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	int local_count = 0;
+
+	if (num == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+
+	recursive_print_hex(num, uppercase, &local_count);
+	return (local_count);
+}
+
+void recursive_print_hex(unsigned int n, int uppercase, int *count)
+{
+	char hex_chars[] = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+	if (n / 16)
+	{
+		recursive_print_hex(n / 16, uppercase, count);
+	}
+	putchar(hex_chars[n % 16]);
+	(*count)++;
+}
