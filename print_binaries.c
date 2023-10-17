bcb1c6e5537babb096e23af4b575fd3f302baad8
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 01:39:41 +0300

    Update print_binaries.c

--- print_binaries.c
+++ print_binaries.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * handle_binary - function to handle binary format
+ * @va_list: list of arguments
+ * Return: count
+ */
 int handle_binary(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
@@ -8,12 +12,16 @@ int handle_binary(va_list args)
 	if (num == 0)
 	{
 		putchar('0');
-		return 1;
+		return (1);
 	}
 	recursive_print_binary(num, &local_count);
-	return local_count;
+	return (local_count);
 }
-
+/**
+ * recursive_print_binary - function to handle recursion
+ * @n: integer n
+ * @count: pointer to count
+ */
 void recursive_print_binary(unsigned int n, int *count)
 {
 	if (n / 2)
