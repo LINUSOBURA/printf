fef59614fdbaada49cecbc8f44665661307f9125
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 02:01:01 +0300

    Update main.h

--- main.h
+++ main.h
@@ -18,6 +18,12 @@ int handle_char(va_list args);
 void recursive_print_integer(int n, int *count);
 int handle_binary(va_list args);
 void recursive_print_binary(unsigned int n, int *count);
+int handle_hex(va_list args, int uppercase);
+void recursive_print_hex(unsigned int n, int uppercase, int *count);
+int handle_unsigned(va_list args);
+void recursive_print_unsigned(unsigned int n, int *count);
+int handle_octal(va_list args);
+void recursive_print_octal(unsigned int n, int *count);
 
 extern FormatHandler handlers[];
 
