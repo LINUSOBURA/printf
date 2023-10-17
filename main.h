485acf987f5430686b9a2ac4a3359b3ee06be514
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 01:36:51 +0300

    Update main.h

--- main.h
+++ main.h
@@ -6,6 +6,15 @@
 #include <stdio.h>
 #include <limits.h>
 
+FormatHandler handlers[] = {
+	{'s', handle_string},
+	{'c', handle_char},
+	{'d', handle_integer},
+	{'i', handle_integer},
+	{'b', handle_binary},
+	{0, NULL}
+};
+
 typedef struct FormatHandler {
 	char specifier;
 	int (*handler)(va_list args);
@@ -16,6 +25,9 @@ int handle_integer(va_list args);
 int handle_string(va_list args);
 int handle_char(va_list args);
 void recursive_print_integer(int n, int *count);
+int handle_binary(va_list args);
+void recursive_print_binary(unsigned int n, int *count);
+
 extern FormatHandler handlers[];
 
 #endif
