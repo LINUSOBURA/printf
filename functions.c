ab89e7d7bc241aef8acb80d8541350dccc1551ef
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-17 23:02:03 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,3 +2,31 @@
 /**
  * handle_string - function to handle string format
  * @va_list: list of arguments
+ */
+void handle_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	if (!str)
+		str = "(null)";
+	while (*str)
+		{
+			putchar(*str);
+			str++;
+			counter++;
+		}
+}
+/**
+ * handle_char - function to handle char format
+ * @va_list: list of arguments
+ */
+void handle_char(va_list args)
+{
+	char c = (char) va_args(args, int);
+	putchar(c);
+}
+
+void handle_d(va_list args)
+{
+	int d = va_arg(args, int);
+	putchar(d);
+}
