37cc4407cf58a0af8228efd0f7c2e6ca1cec01df
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-17 23:16:43 +0300

    Update main.h

--- main.h
+++ main.h
@@ -10,13 +10,6 @@ typedef struct FormatHandler {
 	void (*handler)(va_list args);
 } FormatHandler;
 
-FormatHandler handlers[] = {
-	{'s', handle_string},
-	{'c', handle_char},
-	{'d', handle_d},
-	{'i', handle_i},
-	{0, NULL}
-};
 int _printf(const char *format, ...);
 
 #endif
