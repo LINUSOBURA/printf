8f5c2d0a1b745ebf311730f18461524ad80f975b
LINUSOBURA <linusobura@gmail.com>
2023-10-17 12:35:20 -0700

    add files

---
+++ functions.c
@@ -0,0 +1,4 @@
+#include "main.h"
+/**
+ * handle_string - function to handle string format
+ * @va_list: list of arguments
--- main.h
+++ main.h
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef struct FormatHandler {
+	char specifier;
+	void (*handler)(va_list args);
+} FormatHandler;
+
 int _printf(const char *format, ...);
 
 #endif
