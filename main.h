e326cd43a9d39ff01402cceb7427ef66637561e6
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 00:31:33 +0300

    Update main.h

--- main.h
+++ main.h
@@ -11,10 +11,10 @@ typedef struct FormatHandler {
 } FormatHandler;
 
 int _printf(const char *format, ...);
-void handle_integer(va_list args);
-void handle_string(va_list args);
-void handle_char(va_list args);
-void recursive_print_integer(int n);
+int handle_integer(va_list args);
+int handle_string(va_list args);
+int handle_char(va_list args);
+int recursive_print_integer(int n);
 extern FormatHandler handlers[];
 
 #endif
