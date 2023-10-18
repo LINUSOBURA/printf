ddcb53909e5d2401df94637f289f069634970d33
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 14:09:05 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -40,9 +40,6 @@ int handle_char(va_list args)
 
 void recursive_print_integer(int n, int *count)
 {
-	if (n == 0)
-		return;
-
 	if (n < 0)
 	{
 		putchar('-');
@@ -73,6 +70,12 @@ int handle_integer(va_list args)
 {
 	int num = va_arg(args, int);
 	int local_count = 0;
+
+	if (num == 0)
+	{
+		putchar('0');
+		return(1);
+	}
 
 	recursive_print_integer(num, &local_count);
 	return (local_count);
