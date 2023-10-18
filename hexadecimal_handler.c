595dcfc6a61cac8c3fe19a82bfd361b3ffde0375
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 15:49:15 +0300

    fixed betty

--- hexadecimal_handler.c
+++ hexadecimal_handler.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+* handle_hex - function to handle hexadecimal numbers
+* @args: arguments list
+* @uppercase: integer
+* Return: count
+*/
 int handle_hex(va_list args, int uppercase)
 {
 	unsigned int num = va_arg(args, unsigned int);
@@ -13,7 +19,12 @@ int handle_hex(va_list args, int uppercase)
 	recursive_print_hex(num, uppercase, &local_count);
 	return (local_count);
 }
-
+/**
+* recursive_print_hex - function to handle hexadecimal numbers
+* @n: unsigned integer
+* @uppercase: integer
+* @count: pointer to count
+*/
 void recursive_print_hex(unsigned int n, int uppercase, int *count)
 {
 	char *hex_chars = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
@@ -26,12 +37,22 @@ void recursive_print_hex(unsigned int n, int uppercase, int *count)
 	(*count)++;
 }
 
+/**
+* handle_hex_lower - function to handle hexadecimal numbers
+* @args: arguments list
+* Return: handle hex
+*/
 int handle_hex_lower(va_list args)
 {
-	return handle_hex(args, 0);
+	return (handle_hex(args, 0));
 }
 
+/**
+* handle_hex_upper - function to handle hexadecimal numbers
+* @args: arguments list
+* Return: handle hex
+*/
 int handle_hex_upper(va_list args)
 {
-	return handle_hex(args, 1);
+	return (handle_hex(args, 1));
 }
