9c1df67430c0caa3112ca54e1b1a87130a5b3f80
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 14:41:29 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 				return (count);
 			if (*format == '%' && *(format + 1) == '%')
 			{
-				putchar('%');
+				putchar('%%');
 				count++;
 				continue;
 			}
