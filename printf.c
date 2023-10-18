ee9d343e9b3b4d0f0ecab38a3221bbedfc02e486
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-18 03:05:01 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -10,18 +10,6 @@ int _printf(const char *format, ...)
 {
 	int i, count = 0;
 	va_list args;
-	FormatHandler handlers[] = {
-	{'s', handle_string},
-	{'c', handle_char},
-	{'d', handle_integer},
-	{'i', handle_integer},
-	{'b', handle_binary},
-	{'u', handle_unsigned},
-	{'o', handle_octal},
-	{'x', handle_hex_lower},
-	{'X', handle_hex_upper},
-	{0, NULL}
-};
 
 	va_start(args, format);
 	for (; *format; format++)
